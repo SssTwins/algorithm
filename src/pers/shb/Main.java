314bde80f6e22f009436fd035b1539099323175d
SssTwins <48666037+SssTwins@users.noreply.github.com>
2020-11-12 20:25:28 +0800

    longestPalindromeChildString commit

--- src/pers/shb/Main.java
+++ src/pers/shb/Main.java
@@ -5,6 +5,7 @@ import pers.shb.algorithm.sort.*;
 import pers.shb.exercise.string.DigitalToString;
 
 import java.util.Arrays;
+import java.util.TreeSet;
 
 /**
  * @author SHB
@@ -12,10 +13,11 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] test = new int[]{98,89,79,59,39,24,87,1,8,7,3,45,32,75,24,9,2432,34,566,12,9887,324,235,45,56,232,5235};
-        HeapSort.heapSortImpl(test);
-        System.err.println(Arrays.toString(test));
-        String a="123";
-        System.err.println(DigitalToString.digitalToString(a));
+        TreeSet<Character> a = new TreeSet<>();
+        a.add('d');
+        a.add('a');
+        a.add('b');
+        a.add('c');
+        a.forEach(System.out::println);
     }
 }
---
+++ src/pers/shb/exercise/string/LongestPalindromeChildString.java
@@ -0,0 +1,53 @@
+package pers.shb.exercise.string;
+
+/**
+ * 最长回文子串
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ * <p>
+ * 输入: "cbbd"
+ * 输出: "bb"
+ *
+ * @author SHB
+ * @date 2020年11月12日 19:36:07
+ */
+public class LongestPalindromeChildString {
+    /**
+     * 最长回文子串实现
+     * 动态规划实现，二维数组存储字符串下标范围
+     *
+     * @param s
+     * @return
+     */
+    public static String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        String result = "";
+        for (int len = 0; len < n; len++) {
+            for (int i = 0; i + len < n; i++) {
+                int j = i + len;
+                if (len == 0) {
+                    dp[i][j] = true;
+                } else {
+                    boolean b = s.charAt(i) == s.charAt(j);
+                    if (len == 1) {
+                        dp[i][j] = b;
+                    } else {
+                        //字符串本身是回文串判断首位新增字符是否相等
+                        dp[i][j] = (b && dp[i + 1][j - 1]);
+                    }
+                }
+                if (dp[i][j] && len + 1 > result.length()) {
+                    result = s.substring(i, j + 1);
+                }
+            }
+        }
+        return result;
+    }
+}
