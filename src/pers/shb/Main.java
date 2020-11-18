5de2547b9aedf7464f3677894cd23165979bb844
SssTwins <48666037+SssTwins@users.noreply.github.com>
2020-11-18 19:13:53 +0800

    GasStation MedianOfTwoSortedArrays commit

--- src/pers/shb/Main.java
+++ src/pers/shb/Main.java
@@ -1,23 +1,108 @@
 package pers.shb;
 
-import com.sun.xml.internal.bind.v2.model.annotation.Quick;
-import pers.shb.algorithm.sort.*;
-import pers.shb.exercise.string.DigitalToString;
-
-import java.util.Arrays;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * @author SHB
  */
 public class Main {
-
+    
     public static void main(String[] args) {
-        TreeSet<Character> a = new TreeSet<>();
-        a.add('d');
-        a.add('a');
-        a.add('b');
-        a.add('c');
-        a.forEach(System.out::println);
+        char[][] grid = {
+                {'1', '1', '1', '0', '0'},
+                {'1', '1', '0', '0', '1'},
+                {'1', '0', '0', '1', '1'},
+                {'0', '0', '1', '1', '1'}
+        };
+        System.err.println(numIslands(grid, 6));
+    }
+    
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] result = merge(nums1, nums2);
+        int len = result.length;
+        double res;
+        if (len % 2 == 0) {
+            res = (result[len / 2] + result[len / 2 - 1]) / 2;
+        } else {
+            res = result[len / 2];
+        }
+        return res;
+    }
+    
+    public static int[] merge(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int[] result = new int[len1 + len2];
+        int p = len1-- + len2-- - 1;
+        while (len1 >= 0 && len2 >= 0) {
+            result[p--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
+        }
+        while (len1 >= 0) {
+            result[p--] = nums1[len1--];
+        }
+        while (len2 >= 0) {
+            result[p--] = nums2[len2--];
+        }
+        return result;
+    }
+    
+    /**
+     * 广度优先搜索判断岛屿数量
+     *
+     * @param grid 二维数组
+     * @param s    预计岛屿面积
+     * @return 等于面积的岛屿数量
+     */
+    public static int numIslands(char[][] grid, int s) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int row = grid.length;
+        int col = grid[0].length;
+        //定义临时变量存储岛屿面积
+        int islandArea = 0;
+        int islandNumber = 0;
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (grid[r][c] == '1') {
+                    islandArea++;
+                    grid[r][c] = '0';
+                    Queue<Integer> queue = new LinkedList<>();
+                    queue.add(r * col + c);
+                    while (!queue.isEmpty()) {
+                        int id = queue.remove();
+                        int tmpRow = id / col;
+                        int tmpCol = id % col;
+                        if (tmpRow - 1 >= 0 && grid[tmpRow - 1][tmpCol] == '1') {
+                            queue.add((tmpRow - 1) * col + tmpCol);
+                            islandArea++;
+                            grid[tmpRow - 1][tmpCol] = '0';
+                        }
+                        if (tmpRow + 1 < row && grid[tmpRow + 1][tmpCol] == '1') {
+                            queue.add((tmpRow + 1) * col + tmpCol);
+                            islandArea++;
+                            grid[tmpRow + 1][tmpCol] = '0';
+                        }
+                        if (tmpCol - 1 >= 0 && grid[tmpRow][tmpCol - 1] == '1') {
+                            queue.add((tmpRow) * col + tmpCol - 1);
+                            islandArea++;
+                            grid[tmpRow][tmpCol - 1] = '0';
+                        }
+                        if (tmpCol + 1 < col && grid[tmpRow][tmpCol + 1] == '1') {
+                            queue.add((tmpRow) * col + tmpCol + 1);
+                            islandArea++;
+                            grid[tmpRow][tmpCol + 1] = '0';
+                        }
+                    }
+                    if (islandArea == s) {
+                        islandNumber++;
+                    }
+                    //一个岛屿判断完成后将岛屿面积重置
+                    islandArea = 0;
+                }
+                
+            }
+        }
+        return islandNumber;
     }
 }
---
+++ src/pers/shb/exercise/array/MedianOfTwoSortedArrays.java
@@ -0,0 +1,144 @@
+package pers.shb.exercise.array;
+
+import java.math.BigDecimal;
+
+/**
+ * 给定两个大小为 m 和 n 的正序（从小到大）数组nums1 和nums2。请你找出并返回这两个正序数组的中位数。
+ *
+ * 进阶：你能设计一个时间复杂度为 O(log (m+n)) 的算法解决此问题吗？
+ *
+ * 
+ *
+ * 示例 1：
+ *
+ * 输入：nums1 = [1,3], nums2 = [2]
+ * 输出：2.00000
+ * 解释：合并数组 = [1,2,3] ，中位数 2
+ * 示例 2：
+ *
+ * 输入：nums1 = [1,2], nums2 = [3,4]
+ * 输出：2.50000
+ * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+ * 示例 3：
+ *
+ * 输入：nums1 = [0,0], nums2 = [0,0]
+ * 输出：0.00000
+ * 示例 4：
+ *
+ * 输入：nums1 = [], nums2 = [1]
+ * 输出：1.00000
+ * 示例 5：
+ *
+ * 输入：nums1 = [2], nums2 = []
+ * 输出：2.00000
+ * 
+ *
+ * 提示：
+ *
+ * nums1.length == m
+ * nums2.length == n
+ * 0 <= m <= 1000
+ * 0 <= n <= 1000
+ * 1 <= m + n <= 2000
+ * -106 <= nums1[i], nums2[i] <= 106
+ *
+ * @author SHB
+ * @date 2020年11月15日 09:51:48
+ */
+public class MedianOfTwoSortedArrays {
+    
+    public static  double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] result = merge(nums1,nums2);
+        int len = result.length;
+        double res;
+        if(len%2==0){
+            BigDecimal a = new BigDecimal(result[len/2]);
+            BigDecimal b = new BigDecimal(result[len/2-1]);
+            BigDecimal c = new BigDecimal("2");
+            res = a.add(b).divide(c,10,BigDecimal.ROUND_HALF_UP).doubleValue();
+        }else{
+            res =  result[len/2];
+        }
+        return res;
+    }
+    
+    /**
+     * 合并两个有序数组
+     * @param nums1
+     * @param nums2
+     * @return 合并后的数组
+     */
+    private static int[] merge(int[] nums1,int[] nums2){
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int[] result = new int[len1+len2];
+        int p = len1-- + len2-- - 1;
+        while(len1>=0 && len2>=0){
+            result[p--]=nums1[len1]>nums2[len2]?nums1[len1--]:nums2[len2--];
+        }
+        while(len1>=0){
+            result[p--] = nums1[len1--];
+        }
+        while(len2>=0){
+            result[p--] = nums2[len2--];
+        }
+        return result;
+    }
+    
+    
+    public double sfindMedianSortedArrays(int[] nums1, int[] nums2) {
+        int length1 = nums1.length, length2 = nums2.length;
+        int totalLength = length1 + length2;
+        if (totalLength % 2 == 1) {
+            int midIndex = totalLength / 2;
+            return getKthElement(nums1, nums2, midIndex + 1);
+        } else {
+            int midIndex1 = totalLength / 2 - 1, midIndex2 = totalLength / 2;
+            return (getKthElement(nums1, nums2, midIndex1 + 1) + getKthElement(nums1, nums2, midIndex2 + 1)) / 2.0;
+        }
+    }
+    
+    public int getKthElement(int[] nums1, int[] nums2, int k) {
+        /* 主要思路：要找到第 k (k>1) 小的元素，那么就取 pivot1 = nums1[k/2-1] 和 pivot2 = nums2[k/2-1] 进行比较
+         * 这里的 "/" 表示整除
+         * nums1 中小于等于 pivot1 的元素有 nums1[0 .. k/2-2] 共计 k/2-1 个
+         * nums2 中小于等于 pivot2 的元素有 nums2[0 .. k/2-2] 共计 k/2-1 个
+         * 取 pivot = min(pivot1, pivot2)，两个数组中小于等于 pivot 的元素共计不会超过 (k/2-1) + (k/2-1) <= k-2 个
+         * 这样 pivot 本身最大也只能是第 k-1 小的元素
+         * 如果 pivot = pivot1，那么 nums1[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums1 数组
+         * 如果 pivot = pivot2，那么 nums2[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums2 数组
+         * 由于我们 "删除" 了一些元素（这些元素都比第 k 小的元素要小），因此需要修改 k 的值，减去删除的数的个数
+         */
+        
+        int length1 = nums1.length, length2 = nums2.length;
+        int index1 = 0, index2 = 0;
+        int kthElement = 0;
+        
+        while (true) {
+            // 边界情况
+            if (index1 == length1) {
+                return nums2[index2 + k - 1];
+            }
+            if (index2 == length2) {
+                return nums1[index1 + k - 1];
+            }
+            if (k == 1) {
+                return Math.min(nums1[index1], nums2[index2]);
+            }
+            
+            // 正常情况
+            int half = k / 2;
+            int newIndex1 = Math.min(index1 + half, length1) - 1;
+            int newIndex2 = Math.min(index2 + half, length2) - 1;
+            int pivot1 = nums1[newIndex1], pivot2 = nums2[newIndex2];
+            if (pivot1 <= pivot2) {
+                k -= (newIndex1 - index1 + 1);
+                index1 = newIndex1 + 1;
+            } else {
+                k -= (newIndex2 - index2 + 1);
+                index2 = newIndex2 + 1;
+            }
+        }
+    }
+    
+}
