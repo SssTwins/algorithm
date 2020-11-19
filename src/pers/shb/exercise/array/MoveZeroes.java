948bb556d1c577fe4dd0b8f21c10c082b6805a89
SssTwins <48666037+SssTwins@users.noreply.github.com>
2020-11-19 17:05:12 +0800

    MoveZeroes commit

---
+++ src/pers/shb/exercise/array/MoveZeroes.java
@@ -0,0 +1,41 @@
+package pers.shb.exercise.array;
+
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ * <p>
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ *
+ * @author SHB
+ * @date 2020年11月19日 17:03:33
+ */
+public class MoveZeroes {
+    
+    /**
+     * 移动0
+     *
+     * @param nums 整型数组
+     */
+    public static void moveZeroes(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+    
+    private static void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
